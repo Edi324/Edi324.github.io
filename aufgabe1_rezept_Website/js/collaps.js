9887b42cf2ac82c9c4f350158d50fe6240c72666
Edi324 <ediona.sylejmani@edu.tbz.ch>
2025-03-04 16:06:33 +0100

    added other websties

---
+++ aufgabe1_rezept_Website/js/collaps.js
@@ -0,0 +1,26 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+var coll2 = document.getElementsByClassName("collapsible-2");
+for (i = 0; i < coll2.length; i++) {
+  coll2[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
